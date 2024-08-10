0cb63bf0b599349ebce4c5948ed62a4515d77118
yoshinari0312 <yoshinari0312@keio.jp>
2024-08-10 21:51:38 +0900

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Box, Button } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
 import { useContext } from "react";
 import {Card} from "./components/Card"
 import { Context } from "./components/providers/Provider";
@@ -7,15 +8,22 @@ export const App = () => {
   const {isAdmin, setIsAdmin} = useContext(Context)
   return (
     <Box>
-      {isAdmin?<span>管理者です</span>:<span>管理者以外です</span>}
-      <Button  variant="outlined"
-        onClick={() => {
-          setIsAdmin(!isAdmin)
-        }}
-      >
-        切り替え
-      </Button>
-      <Box display="flex" justifyContent="center" alignItems="center">
+      <Grid container>
+        <Grid xs={2} sx={{marginLeft:"10px"}}>
+          {isAdmin?<p>管理者です</p>:<p>管理者以外です</p>}
+        </Grid>
+        <Grid xs={2} sx={{marginTop:"10px"}}>
+          <Button  variant="outlined"
+            onClick={() => {
+              setIsAdmin(!isAdmin)
+            }}
+          >
+            切り替え
+          </Button>
+        </Grid>
+        <Grid xs={8}></Grid>
+      </Grid>
+      <Box display="flex" justifyContent="center">
         <Card/>
       </Box>
     </Box>
